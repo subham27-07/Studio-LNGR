dff6ec64b3c9e33003ab8f7c176dc9bdaece5268
subham27-07 <82700165+subham27-07@users.noreply.github.com>
2022-02-13 16:07:45 -0500

    Update series.py

--- series.py
+++ series.py
@@ -6,6 +6,8 @@ import adafruit_sht4x
 
 import adafruit_sgp30
 
+import adafruit_sht31d
+
 # Create I2C bus as normal
 i2c = board.I2C()  # uses board.SCL and board.SDA
 
@@ -16,6 +18,9 @@ tca = adafruit_tca9548a.TCA9548A(i2c)
 #tsl1= adafruit_sgp30.Adafruit_SGP30(tca[0])
 tsl1= adafruit_sht4x.SHT4x(tca[0])
 tsl2= adafruit_sht4x.SHT4x(tca[1])
+tsl3= adafruit_sht31d.SHT31D(tca[2])
+
+
 
 tsl1.mode = adafruit_sht4x.Mode.NOHEAT_HIGHPRECISION
 tsl2.mode = adafruit_sht4x.Mode.NOHEAT_HIGHPRECISION
@@ -32,6 +37,13 @@ while True:
     print("Temperature2: %0.1f C" % temperature2)
     print("Humidity2: %0.1f %%" % relative_humidity2)
     print("")
+
+    # print("\nTemperature: %0.1f C" % tsl3.temperature)
+    print("Humidity: %0.1f %%" % tsl3.relative_humidity)
+    tsl3.heater = True
+    print("Sensor Heater status =", tsl3.heater)
+
+
     time.sleep(1)
 
 
